a5e6d7d78df3567478ba02e1ed76fe9254302b54
Thai Ha <thaihanguyen98@gmail.com>
2024-11-26 18:58:15 +1100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-export default function Footer() {
-  return <div className="Footer"></div>;
-}
+const Footer = () => {
+  return (
+    <Box mt={4} textAlign="center">
+      <Typography variant="caption">&copy; To Do List App-Group4 </Typography>
+      <Typography variant="caption">&copy; TBSP </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
