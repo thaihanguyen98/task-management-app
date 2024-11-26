64742ef555ad415bfd8c7ca3ef39ccdef282c56b
Thai Ha <thaihanguyen98@gmail.com>
2024-11-26 19:01:07 +1100

    Update TaskStatusButton.js

--- src/components/TaskStatusButton.js
+++ src/components/TaskStatusButton.js
@@ -1,5 +1,27 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
-export default function TaskStatusButton() {
-  return <div className="TaskStatusButton"></div>;
-}
+const TaskStatusButton = ({ currentStatus, onButtonClick }) => {
+  const getButtonLabel = () => {
+    switch (currentStatus) {
+      case "not-started":
+        return "Accept Task";
+      case "in-progress":
+        return "Mark to Review";
+      case "review":
+        return "Mark Completed";
+      case "completed":
+        return "Reset Task"; // or any other relevant action
+      default:
+        return "Accept Task";
+    }
+  };
+
+  return (
+    <Button variant="outlined" onClick={onButtonClick}>
+      {getButtonLabel()}
+    </Button>
+  );
+};
+
+export default TaskStatusButton;
